474845d7e6098207ffefaa81f273fd5871c7ff82
David Guyon <david@guyon.me>
2015-03-30 16:12:43 +0200

    Add: prepare and FRIEDA tasks

--- config/deploy/xp5k_common_tasks.rb
+++ config/deploy/xp5k_common_tasks.rb
@@ -44,4 +44,39 @@ task :describe do
   end
 end
 
+desc 'Prepare my stuff'
+task :prepare, :roles => [:controller] do
+  set :user, "root"
+  set :default_environment, { 
+    "http_proxy"  => "http://proxy:3128",
+    "https_proxy" => "http://proxy:3128",
+    "OS_USERNAME" => "test",
+    "OS_PASSWORD" => "abc123",
+    "OS_TENANT_NAME" => "test",
+    "OS_AUTH_URL" => "http://localhost:5000/v2.0/"
+  }
+  run "apt-get install --yes git && 
+    git clone https://davidguyon@bitbucket.org/davidguyon/greenerbar.git && 
+    cd greenerbar &&
+    bash install-greenerbar.bash &&
+    unset http_proxy https_proxy &&
+    nova keypair-add test_key > ~/.ssh/test_key.pem &&
+    chmod 600 ~/.ssh/test_key.pem &&
+    echo 'export EC2_KEYPAIR=test_key' >> ~/.bashrc &&
+    echo 'export EC2_SECURITY_GROUP=vm_jdoe_sec_group' >> ~/.bashrc"
+end
+
+desc 'Send FRIEDA on controller'
+task :FRIEDA, :roles => [:controller] do
+  set :user, "root"
+  set :default_environment, { 
+    "http_proxy"  => "http://proxy:3128",
+    "https_proxy" => "http://proxy:3128"
+  }
+  upload "/home/dguyon/FRIEDA/", "FRIEDA", :via => :scp, :recursive => :true
+  run "cd FRIEDA &&
+    ./setup.py build &&
+    ./setup.py install"
+end
+
 after "xp5k", "submit", "deploy"
